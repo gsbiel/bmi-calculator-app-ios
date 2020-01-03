61ae34e62b09bdcf2b46fa9b505eeab671a17ca7
user161760 <gabriel.sgaspar@yahoo.com.br>
2020-01-03 08:12:52 -0500

    calculo do IBM feito

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onHeightChange(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f m", sender.value)
+    }
+    
+    @IBAction func onWeightChange(_ sender: UISlider) {
+        weightLabel.text = String(format: "%d kg", Int(sender.value))
+    }
+    
+    
+    @IBAction func onCalculatePressed(_ sender: UIButton) {
+        let ibm = weightSlider.value / (heightSlider.value*heightSlider.value)
+        print(String(format: "%.2f", ibm))
+    }
 }
 
